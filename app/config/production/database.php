b2a9daba9dbb5c23247cbbbfe574f785bf70fced
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2014-08-07 13:51:22 -0400

    database

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -8,8 +8,8 @@ return array(
             'driver'    => 'mysql',
             'host'      => 'tunnel.pagodabox.com:3306',
             'database'  => 'p4',
-            'username'  => 'norene',
-            'password'  => 'vNQhV9Pw',
+            'username'  => 'sheilah',
+            'password'  => 'pAoG0DWw',
         ),
 
     ),
